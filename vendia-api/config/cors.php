eda1c6249c57a46c882d8fb31d4b73d6c8f76b47
Aommy KjpN <aommykjpn@MacminiAommy.local>
2026-04-05 19:29:21 +0700

    fixed cors

--- vendia-api/config/cors.php
+++ vendia-api/config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:8081'],
+    'allowed_origins' => array_values(array_filter(array_map(
+        'trim',
+        explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://localhost:8081'))
+    ))),
 
     'allowed_origins_patterns' => [],
 
